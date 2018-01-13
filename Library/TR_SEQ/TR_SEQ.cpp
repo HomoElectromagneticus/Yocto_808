222fe7f29087ec8c51d730fd1473ca682e410396
Homo Electromagneticus <homoelectromagneticus@protonmail.com>
2018-01-12 21:19:56 -0800

    Fixed some poorly formed bit manipulation instructions in the sequencer driver

--- Library/TR_SEQ/TR_SEQ.cpp
+++ Library/TR_SEQ/TR_SEQ.cpp
@@ -44,23 +44,24 @@ uint8_t TR_SEQ::shiftIn_SRIO()
   return myDataIn;*/
   byte value = 0;
 
-        for (byte i = 0; i <8; i++) {
-                value = value | (bitRead(PINB,6)<<(7-i));//value |  (digitalRead(6) << (7-i));
-                PORTB |= (1<<7);//digitalWrite(7, HIGH);
-                PORTB &= ~(1<<7);// digitalWrite(7, LOW);
-        }
-        return value;
+  for (byte i = 0; i <8; i++) {
+    value = value | (bitRead(PINB,6)<<(7-i));//value |  (digitalRead(6) << (7-i));
+    PORTB |= (1<<7);        //digitalWrite(7, HIGH);
+    // delayMicroseconds(20); ???
+    PORTB &= ~(1<<7);       //digitalWrite(7, LOW);
+  }
+  return value;
 }
 
 void TR_SEQ::shiftOut_SRIO(uint8_t val)
 {
 	uint8_t i;
 
-	for (i = 0; i < 8; i++)  {
-		
-			bitWrite(PORTB,5,!!(val & (1 << (7 - i))));
-		PORTB |=  1<<7; //clockPin 9 high
-        PORTB &= ~1<<7;//clockPin 9 low
+	for (i = 0; i < 8; i++) {
+		bitWrite(PORTB,5,!!(val & (1 << (7 - i))));
+		PORTB |= 1<<7;         //clockPin 9 high
+    //
+    PORTB &= ~(1<<7);       //clockPin 9 low
 	}
 }
 
@@ -68,13 +69,12 @@ void TR_SEQ::Led_All_On()
 {
   // turn on all the LEDs
   //Allume toutes les Leds
-  PORTB &= 0<<4;//latchPinOut 12 low
+  PORTB &= (0<<4);          //latchPinOut 12 low
   shiftOut_SRIO(255);
   shiftOut_SRIO(255);
-  PORTB |= 1<<4;//latchPinOut 12 high
-  for(byte i=0;i<8;i++)
-  {
-  _SR[i]=255;
+  PORTB |= 1<<4;            //latchPinOut 12 high
+  for(byte i=0;i<8;i++) {
+    _SR[i]=255;
   }
 }
 
@@ -82,26 +82,25 @@ void TR_SEQ::Led_All_Off()
 {
   // turn off all the LEDs
   //Eteint toutes les Leds
-  PORTB &= 0<<4;//latchPinOut 12 low
+  PORTB &= (0<<4);          //latchPinOut 12 low
   shiftOut_SRIO(0);
   shiftOut_SRIO(0);
-  PORTB |= 1<<4;//latchPinOut 12 high
-  for(byte i=0;i<8;i++)
-  {
-  _SR[i]=0;
+  PORTB |= 1<<4;            //latchPinOut 12 high
+  for(byte i=0;i<8;i++) {
+    _SR[i]=0;
   }
 }
  
 void TR_SEQ::Led_SR_Write (byte SR_num, byte data)
 {
   // function that sends one byte to the selected shift register
-  // Fonction qui envoie un octet au SR selectionné
+  // Fonction qui envoie un octet au SR selectionnï¿½
   _SR[SR_num]=data;
-  PORTB &= 0<<4;//latchPinOut 12 low
+  PORTB &= (0<<4);          //latchPinOut 12 low
   
   shiftOut_SRIO(_SR[1]);
   shiftOut_SRIO(_SR[0]);  
-  PORTB |= 1<<4;//latchPinOut 12 high
+  PORTB |= 1<<4;            //latchPinOut 12 high
 } 
 
 void TR_SEQ:: Led_Step_Write(uint16_t data)
@@ -109,13 +108,13 @@ void TR_SEQ:: Led_Step_Write(uint16_t data)
   _SR[0]=data;
   _SR[1]=data>>8;
   // function that sends the state of the LEDS to edit
-  //Fonction qui envoi l'état des led pour editer
-  PORTB &= 0<<4;//digitalWrite(_latchPinOut, 0);
-    shiftOut_SRIO(_SR[3]);
-    shiftOut_SRIO(_SR[2]);
-    shiftOut_SRIO(data>>8);
-    shiftOut_SRIO(data);
-  PORTB |= 1<<4;//digitalWrite(_latchPinOut, 1);
+  //Fonction qui envoi l'ï¿½tat des led pour editer
+  PORTB &= (0<<4);          //digitalWrite(_latchPinOut, 0);
+  shiftOut_SRIO(_SR[3]);
+  shiftOut_SRIO(_SR[2]);
+  shiftOut_SRIO(data>>8);
+  shiftOut_SRIO(data);
+  PORTB |= 1<<4;            //digitalWrite(_latchPinOut, 1);
 }
 
 void TR_SEQ:: ShiftOut_Update(uint16_t data_led,uint16_t data_inst)
@@ -125,50 +124,52 @@ void TR_SEQ:: ShiftOut_Update(uint16_t data_led,uint16_t data_inst)
   _SR[2]=data_inst;
   _SR[1]=data_inst>>8;
   // function that sends the state of the LEDS to edit
-  //Fonction qui envoi l'état des led pour editer
-  PORTB &= 0<<4;//digitalWrite(_latchPinOut, 0);
-    shiftOut_SRIO(data_inst>>8);
-    shiftOut_SRIO(data_inst);
-    shiftOut_SRIO(data_led>>8);
-    shiftOut_SRIO(data_led);
-  PORTB |= 1<<4;//digitalWrite(_latchPinOut, 1);
+  //Fonction qui envoi l'ï¿½tat des led pour editer
+  PORTB &= (0<<4);            //digitalWrite(_latchPinOut, 0);
+  shiftOut_SRIO(data_inst>>8);
+  shiftOut_SRIO(data_inst);
+  shiftOut_SRIO(data_led>>8);
+  shiftOut_SRIO(data_led);
+  PORTB |= 1<<4;              //digitalWrite(_latchPinOut, 1);
 }
+
 void TR_SEQ:: Inst_Send(uint16_t data)
 {  
   _SR[2]=data;
   _SR[3]=data>>8;
   // function that sends to the shift register which instrument needs to be triggered
   //Fonction qui envoi au shift register quel instrument doit etre trigger
-  PORTB &= 0<<4;//digitalWrite(_latchPinOut, 0);
-    shiftOut_SRIO(data>>8);
-    shiftOut_SRIO(data);
+  PORTB &= (0<<4);            //digitalWrite(_latchPinOut, 0);
+  shiftOut_SRIO(data>>8);
+  shiftOut_SRIO(data);
 	shiftOut_SRIO(_SR[1]);
 	shiftOut_SRIO(_SR[0]);
-  PORTB |= 1<<4;//digitalWrite(_latchPinOut, 1);
+  PORTB |= 1<<4;              //digitalWrite(_latchPinOut, 1);
 }
+
 byte TR_SEQ::Led_SR_Read (byte SR_num)
 { 
   // function that returns the state of the byte to the selected shift register
-  // Fonction qui renvoi l'état de l'octet du SR sélectionné
+  // Fonction qui renvoi l'ï¿½tat de l'octet du SR sï¿½lectionnï¿½
   return _SR[SR_num];
 }
 
 void TR_SEQ::Led_Pin_Write (byte Pin_num, byte flag)
 {
   // function which sends the state of the LED byte data
-  // Fonction qui envoi l'état de le led  byte data;
+  // Fonction qui envoi l'ï¿½tat de le led  byte data;
   bitWrite(_SR[Pin_num/8],Pin_num-(8*(Pin_num/8)),flag);
-  PORTB &= 0<<4;//digitalWrite(_latchPinOut, 0);
+  PORTB &= (0<<4);            //digitalWrite(_latchPinOut, 0);
   shiftOut_SRIO(_SR[1]);
   shiftOut_SRIO(_SR[0]);
-  PORTB |= 1<<4;//digitalWrite(_latchPinOut, 1);
+  PORTB |= 1<<4;              //digitalWrite(_latchPinOut, 1);
 }
     
 byte TR_SEQ::Button_SR_Read (byte SR_num)
 {
   // function that returns in the form of bytes the state of the buttons of the
   // selected shift register
-  // Fonction qui renvoie sous forme d'octet l'état des boutons du SR sélectionné
+  // Fonction qui renvoie sous forme d'octet l'ï¿½tat des boutons du SR sï¿½lectionnï¿½
   digitalWrite(_latchPinIn,1);
   //delayMicroseconds(20);
   
@@ -193,12 +194,12 @@ byte TR_SEQ::Button_Pin_Read (byte Pin_num){
 
   digitalWrite(_latchPinIn,0);
   
-    if (bitRead (_data[Pin_num/8], Pin_num-(8*(Pin_num/8)) )){
-      flag =1;
-    }
-    else{
-      flag =0;
-    }
+  if (bitRead (_data[Pin_num/8], Pin_num-(8*(Pin_num/8)) )){
+    flag =1;
+  }
+  else{
+    flag =0;
+  }
   return flag;
 }
 
@@ -211,7 +212,6 @@ int TR_SEQ:: Button_Step_Read()
   digitalWrite(_latchPinIn,1);
   //delayMicroseconds(20);
   
-  
   firstbyte =shiftIn_SRIO();
   secondbyte=shiftIn_SRIO();
   
@@ -220,4 +220,3 @@ int TR_SEQ:: Button_Step_Read()
   word16bits = firstbyte | secondbyte <<8;
   return word16bits;
 }  
- 
