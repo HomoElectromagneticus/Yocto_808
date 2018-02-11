70c5a5ec55bb8c1b5b3b7895f473d451b32d5ef0
Homo Electromagneticus <homoelectromagneticus@protonmail.com>
2018-02-11 00:36:50 -0800

    Cleaned up the code style a bit to improve the legibility

--- Yocto/BPM_fonction.ino
+++ Yocto/BPM_fonction.ino
@@ -18,58 +18,58 @@ uint16_t TapTempoBpm = TEMPO_MIN;
 // function that checks the encoder and returns the time of the timer
 //Fonction qui check l'encoder et renvoi le temp dans le timer//
 void Check_BPM() {
-  //Local Variables
-  int8_t tmpdata;
-  //Appel la fonction qui lit l'encoder (call the function to read the encoder)
-  tmpdata = Read_Encoder();
-  if(!roll_mode) {
-    //l'encoder a t il bouger (has the encoder moved?)
-    if(tmpdata) {
-      //Serial.print("Tempo =");
-      //Serial.println(bpm/4, DEC);
-      //Set the new counter value of the encoder
-      bpm -= tmpdata;  
-      bpm = constrain(bpm,200,1000); 
-      timer_time = ((unsigned int)(2500000/bpm));
-      //Serial.println(timer_time);
-      Timer1.initialize(timer_time); // set a timer of length in microseconds
+    //Local Variables
+    int8_t tmpdata;
+    //Appel la fonction qui lit l'encoder (call the function to read the encoder)
+    tmpdata = Read_Encoder();
+    if(!roll_mode) {
+        //l'encoder a t il bouger (has the encoder moved?)
+        if(tmpdata) {
+            //Serial.print("Tempo =");
+            //Serial.println(bpm/4, DEC);
+            //Set the new counter value of the encoder
+            bpm -= tmpdata;  
+            bpm = constrain(bpm,200,1000); 
+            timer_time = ((unsigned int)(2500000/bpm));
+            //Serial.println(timer_time);
+            Timer1.initialize(timer_time); // set a timer of length in microseconds
+        }
     }
-  }
 }
 
 
 void Check_Roll_Scale() {
-  //Local Variables
-  int8_t tmp_data;
-  //Appel la fonction qui lit l'encoder (call the function to read the encoder)
-  tmp_data = Read_Encoder();
+    //Local Variables
+    int8_t tmp_data;
+    //Appel la fonction qui lit l'encoder (call the function to read the encoder)
+    tmp_data = Read_Encoder();
 
-  //l'encoder a t il bouger (has the encoder moved?)
-  if (tmp_data) {
-    if (roll_mode) {
-      temp_roll_scale -= tmp_data;
-      temp_roll_scale = constrain(temp_roll_scale,1,30);
-	    // if we are in "PATTERN_MIDI_MASTER"
-      if (selected_mode == 0) {//si on est en PATTERN_MIDI_MASTER
-        roll_pointer = map(temp_roll_scale,0,30,0,3);
-      }
-      else {
-		    // we map only between 0 and 2 in slave mode
-        roll_pointer = map(temp_roll_scale,0,30,0,2);//on map que entre 0 et 2 en mode slave
-      }
-      Serial.println(temp_roll_scale);
+    //l'encoder a t il bouger (has the encoder moved?)
+    if (tmp_data) {
+        if (roll_mode) {
+            temp_roll_scale -= tmp_data;
+            temp_roll_scale = constrain(temp_roll_scale,1,30);
+	        // if we are in "PATTERN_MIDI_MASTER"
+          if (selected_mode == 0) {//si on est en PATTERN_MIDI_MASTER
+              roll_pointer = map(temp_roll_scale,0,30,0,3);
+          }
+          else {
+		          // we map only between 0 and 2 in slave mode
+              roll_pointer = map(temp_roll_scale,0,30,0,2);//on map que entre 0 et 2 en mode slave
+          }
+        Serial.println(temp_roll_scale);
+        }
     }
-  }
 }
 
 
 int8_t Read_Encoder() {
-  int8_t enc_states[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
-  static uint8_t old_AB = 0;
-  /**/
-  old_AB <<= 2;                   //remember previous state
-  old_AB |= ( ENC_PORT>>6 & 0x03 );  //add current state
-  return ( enc_states[( old_AB & 0x0f )]);
+    int8_t enc_states[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
+    static uint8_t old_AB = 0;
+    /**/
+    old_AB <<= 2;                       //remember previous state
+    old_AB |= ( ENC_PORT>>6 & 0x03 );   //add current state
+    return ( enc_states[( old_AB & 0x0f )]);
 }
 
 
--- Yocto/Midi_Play.ino
+++ Yocto/Midi_Play.ino
@@ -41,42 +41,41 @@
 
 
 void Handle_NoteOn(byte channel, byte pitch, byte velocity) {
-  if (velocity == 0){
-    return;
-  }
+    if (velocity == 0){
+        return;
+    }
+    if (midi_led_flash_count == 0) {
+        PORTC |= MIDI_ACTIVITY_LED;
+    }
+    midi_led_flash_count = 10;
 
-  if (midi_led_flash_count == 0) {
-    PORTC |= MIDI_ACTIVITY_LED;
-  }
-  midi_led_flash_count = 10;
+    //on enregistre le numero du noteOn dans l'instrument selectionné
+    if(button_shift) {
+        inst_midi_note[selected_inst] = pitch;
+        inst_midi_note_edited = 1;
+    }
 
-  //on enregistre le numero du noteOn dans l'instrument selectionné
-  if(button_shift) {
-    inst_midi_note[selected_inst] = pitch;
-    inst_midi_note_edited = 1;
-  }
+    if (pitch == inst_midi_note[0]) bitSet(inst_midi_buffer, 0);
+    if (pitch == inst_midi_note[1]) bitSet(inst_midi_buffer, 1);
+    if (pitch == inst_midi_note[2]) bitSet(inst_midi_buffer, 2);
+    if (pitch == inst_midi_note[3]) bitSet(inst_midi_buffer, 3);
+    if (pitch == inst_midi_note[4]) bitSet(inst_midi_buffer, 4);
+    if (pitch == inst_midi_note[5]) bitSet(inst_midi_buffer, 5);
+    if (pitch == inst_midi_note[6]) bitSet(inst_midi_buffer, 6);
+    if (pitch == inst_midi_note[7]) bitSet(inst_midi_buffer, 7);
+    if (pitch == inst_midi_note[8]) bitSet(inst_midi_buffer, 8);
+    if (pitch == inst_midi_note[9]) bitSet(inst_midi_buffer, 9);
+    if (pitch == inst_midi_note[10]) bitSet(inst_midi_buffer, 10);
+    if (pitch == inst_midi_note[11]) bitSet(inst_midi_buffer, 11);
+    if (pitch == inst_midi_note[12]) bitSet(inst_midi_buffer, 12);
+    if (pitch == inst_midi_note[13]) bitSet(inst_midi_buffer, 13);
+    if (pitch == inst_midi_note[14]) bitSet(inst_midi_buffer, 14);
+    if (pitch == inst_midi_note[15]) bitSet(inst_midi_buffer, 15);
 
-  if (pitch == inst_midi_note[0]) bitSet(inst_midi_buffer, 0);
-  if (pitch == inst_midi_note[1]) bitSet(inst_midi_buffer, 1);
-  if (pitch == inst_midi_note[2]) bitSet(inst_midi_buffer, 2);
-  if (pitch == inst_midi_note[3]) bitSet(inst_midi_buffer, 3);
-  if (pitch == inst_midi_note[4]) bitSet(inst_midi_buffer, 4);
-  if (pitch == inst_midi_note[5]) bitSet(inst_midi_buffer, 5);
-  if (pitch == inst_midi_note[6]) bitSet(inst_midi_buffer, 6);
-  if (pitch == inst_midi_note[7]) bitSet(inst_midi_buffer, 7);
-  if (pitch == inst_midi_note[8]) bitSet(inst_midi_buffer, 8);
-  if (pitch == inst_midi_note[9]) bitSet(inst_midi_buffer, 9);
-  if (pitch == inst_midi_note[10]) bitSet(inst_midi_buffer, 10);
-  if (pitch == inst_midi_note[11]) bitSet(inst_midi_buffer, 11);
-  if (pitch == inst_midi_note[12]) bitSet(inst_midi_buffer, 12);
-  if (pitch == inst_midi_note[13]) bitSet(inst_midi_buffer, 13);
-  if (pitch == inst_midi_note[14]) bitSet(inst_midi_buffer, 14);
-  if (pitch == inst_midi_note[15]) bitSet(inst_midi_buffer, 15);
-
-  if (inst_midi_buffer > 0 && velocity >= 100) {
-    // Enable accent
-    bitSet(inst_midi_buffer,0);
-  }
+    if (inst_midi_buffer > 0 && velocity >= 100) {
+        // Enable accent
+        bitSet(inst_midi_buffer,0);
+    }
 }
 
 
@@ -85,156 +84,155 @@ void Handle_NoteOff(byte channel, byte pitch, byte velocity) {
 
 
 void Handle_Start() {
-  PORTD |= (1<<5);// met au niveau haut le sorti Din start
-  play=1;
-  ppqn_count=0;
-  first_play=1;
+    PORTD |= (1<<5);// met au niveau haut le sorti Din start
+    play = 1;
+    ppqn_count = 0;
+    first_play = 1;
 }
 
 
 void Handle_Stop() {
-  PORTD &= ~(1<<5);//met a 0 la sorti DIN start
-  play=0;
-  step_count=0;
-  PORTB &= ~(1<<2);// met a 0 la sorti TRIG CPU
+    PORTD &= ~(1<<5);//met a 0 la sorti DIN start
+    play = 0;
+    step_count = 0;
+    PORTB &= ~(1<<2);// met a 0 la sorti TRIG CPU
 }
 
 
 void Handle_Clock() {
-  Reset_Trig_Out();
-  PORTD |= (1<<4);//met a 1 la clock DIN
-  delayMicroseconds (1000);//delay neccessaire pour que le cycle de la clock soit correct en DIN_CLK
-  PORTD &= ~(1<<4);//met a o la clock DIN
-  //bitWrite(PORTD,4,!(bitRead (PIND,4)));
-  //PORTB &= ~(1<<2);// met a 0 la sorti TRIG CPU
+    Reset_Trig_Out();
+    PORTD |= (1<<4);//met a 1 la clock DIN
+    delayMicroseconds (1000);//delay neccessaire pour que le cycle de la clock soit correct en DIN_CLK
+    PORTD &= ~(1<<4);//met a o la clock DIN
+    //bitWrite(PORTD,4,!(bitRead (PIND,4)));
+    //PORTB &= ~(1<<2);// met a 0 la sorti TRIG CPU
 
-
-  //MODE ROLL
-  if(roll_mode && ppqn_count%(roll_scale[scale_type][roll_pointer]/4) == 0 && inst_roll>0){//
-    PORTB |= (1<<2);//envoie une impulsion sur la sorti trig CPU a chaque pas
-    //SR.ShiftOut_Update(temp_step_led,inst_roll);
-    //Send_Trig_Out();
-  }
-  if(step_changed) {
-    SR.ShiftOut_Update(temp_step_led,((inst_step_buffer[step_count][pattern_buffer])&(~inst_mute)|inst_roll));
-    Send_Trig_Out();
-    step_changed=0;
-	PORTB |= (1<<2);//envoie une impulsion sur la sorti trig CPU a chaque pas
-  }
-  else {
-    SR.ShiftOut_Update(temp_step_led,inst_roll);
-  }
-
-  ppqn_count++;
-  tempo_led_count++;
-
-  //PLAY=================================================================
-  if(play) {
-    //Update clignotement des leds
-    if (tempo_led_count >= 12) {
-      tempo_led_count = 0;//si le compteur egale un temps on le reinitialise
-      tempo_led_flag_block =! tempo_led_flag_block;//clignote au tempo
+    //MODE ROLL
+    if(roll_mode && ppqn_count%(roll_scale[scale_type][roll_pointer]/4) == 0 && inst_roll>0) {
+        PORTB |= (1<<2);//envoie une impulsion sur la sorti trig CPU a chaque pas
+        //SR.ShiftOut_Update(temp_step_led,inst_roll);
+        //Send_Trig_Out();
     }
-    if(ppqn_count >= 3) tempo_led_flag = 0;//on alterne la valeur du flag de la led tempo.
-    else tempo_led_flag = 1;
-
-    if (first_play) {
-	  ppqn_count=0;//initialise le compteur PPQN
-      PORTB |= 1<<2;//envoie une impulsion sur la sorti trig CPU a chaque pas
-      SR.ShiftOut_Update(temp_step_led,(inst_step_buffer[0][pattern_buffer])&(~inst_mute));
-      Send_Trig_Out();
-      first_play = 0;//initialise le flag
+    if(step_changed) {
+        SR.ShiftOut_Update(temp_step_led,((inst_step_buffer[step_count][pattern_buffer])&(~inst_mute)|inst_roll));
+        Send_Trig_Out();
+        step_changed = 0;
+	      PORTB |= (1<<2);//envoie une impulsion sur la sorti trig CPU a chaque pas
     }
-    if (ppqn_count >= pattern_scale[pattern_buffer]/4) {
-      ppqn_count = 0;
-      step_count++;
-      if (step_count == (nbr_step[pattern_buffer]-1)) {
-        middle_mesure_flag = 1;//indique le mileu de la meusure enfin que la mesure est avancé
-        //MODE PATTERN PLAY--------------------------------------------------------------------------------------------------------
+    else {
+        SR.ShiftOut_Update(temp_step_led,inst_roll);
+    }
+
+    ppqn_count++;
+    tempo_led_count++;
+
+    //PLAY=================================================================
+    if(play) {
+        //Update clignotement des leds
+        if (tempo_led_count >= 12) {
+            tempo_led_count = 0;//si le compteur egale un temps on le reinitialise
+            tempo_led_flag_block =! tempo_led_flag_block;//clignote au tempo
+        }
+        if(ppqn_count >= 3) tempo_led_flag = 0;//on alterne la valeur du flag de la led tempo.
+        else tempo_led_flag = 1;
+
+        if (first_play) {
+	          ppqn_count = 0;   //initialise le compteur PPQN
+            PORTB |= 1<<2;    //envoie une impulsion sur la sorti trig CPU a chaque pas
+            SR.ShiftOut_Update(temp_step_led,(inst_step_buffer[0][pattern_buffer])&(~inst_mute));
+            Send_Trig_Out();
+            first_play = 0;   //initialise le flag
+        }
+        if (ppqn_count >= pattern_scale[pattern_buffer]/4) {
+            ppqn_count = 0;
+            step_count++;
+            if (step_count == (nbr_step[pattern_buffer]-1)) {
+                middle_mesure_flag = 1;//indique le mileu de la meusure enfin que la mesure est avancé
+                //MODE PATTERN PLAY--------------------------------------------------------------------------------------------------------
+                if(selected_mode == PATTERN_MIDI_MASTER || selected_mode == PATTERN_MIDI_SLAVE || selected_mode == PATTERN_DIN_SLAVE) {
+                    pattern_count++;//compte les mesure pour faire avance les pattern en mode song ou quand un block de pattern est selectionner
+                    if(pattern_count > nbr_pattern_block){//on reset le comteur quand il est superieur au nombre de pattern a lire dans le block
+                        pattern_count = 0;
+                    }
+                    if (nbr_pattern_block_changed_A) {
+                        nbr_pattern_block_changed_A = 0;
+                        pattern_count = 0;
+                    }
+                }
+                //MODE SONG PLAY--------------------------------------------------------------------------------------------------------
+                if(selected_mode == SONG_MIDI_MASTER || selected_mode == SONG_MIDI_SLAVE || selected_mode == SONG_DIN_SLAVE) {
+                    pattern_count++;//compte les mesure pour faire avance les pattern en mode song ou quand un block de pattern est selectionner
+                    if(pattern_count >= total_pattern_song[song_buffer]) {//on reset le comteur quand il est superieur au nombre de pattern dans le song
+                        pattern_count = 0;
+                    }
+                }
+            }
+            step_changed = 1;       //flag que le pas a change
+            if(step_count >= nbr_step[pattern_buffer]) {
+                step_count = 0;
+                if(load_pattern_ok) {
+                    pattern_buffer =! pattern_buffer;//permet de switcher entre les deux pattern present dans le buffer au debut de la mesure
+                    load_pattern_ok = 0;
+                }
+            }
+        } 
+    }
+    //STOP=================================================================
+    else if (!play) {
+        if (tempo_led_count >= 12) {
+            tempo_led_count = 0;    //si le compteur egale un temps on le reinitialise
+            tempo_led_flag =! tempo_led_flag;//on alterne la valeur du flag de la led tempo.
+        }
         if(selected_mode==PATTERN_MIDI_MASTER || selected_mode==PATTERN_MIDI_SLAVE || selected_mode==PATTERN_DIN_SLAVE){
-          pattern_count++;//compte les mesure pour faire avance les pattern en mode song ou quand un block de pattern est selectionner
-          if(pattern_count>nbr_pattern_block){//on reset le comteur quand il est superieur au nombre de pattern a lire dans le block
-            pattern_count=0;
-          }
-          if (nbr_pattern_block_changed_A) {
-            nbr_pattern_block_changed_A = 0;
-            pattern_count = 0;
-          }
+            pattern_count = 0;      //reinitilise le compteur du block pattern
         }
-        //MODE SONG PLAY--------------------------------------------------------------------------------------------------------
-        if(selected_mode==SONG_MIDI_MASTER || selected_mode==SONG_MIDI_SLAVE || selected_mode==SONG_DIN_SLAVE){
-          pattern_count++;//compte les mesure pour faire avance les pattern en mode song ou quand un block de pattern est selectionner
-          if(pattern_count >= total_pattern_song[song_buffer]){//on reset le comteur quand il est superieur au nombre de pattern dans le song
-            pattern_count = 0;
-          }
+        if((selected_mode==SONG_MIDI_MASTER || selected_mode==SONG_MIDI_SLAVE || selected_mode==SONG_DIN_SLAVE)&&(button_reset)){
+            pattern_count = 0;      //reinitilise le compteur de position du song
         }
-      }
-      step_changed = 1;       //flag que le pas a change
-      if(step_count >= nbr_step[pattern_buffer]) {
-        step_count = 0;
-        if(load_pattern_ok) {
-          pattern_buffer =! pattern_buffer;//permet de switcher entre les deux pattern present dans le buffer au debut de la mesure
-          load_pattern_ok = 0;
-        }
-      }
-    } 
-  }
-  //STOP=================================================================
-  else if (!play) {
-    if (tempo_led_count >= 12) {
-      tempo_led_count = 0;    //si le compteur egale un temps on le reinitialise
-      tempo_led_flag =! tempo_led_flag;//on alterne la valeur du flag de la led tempo.
     }
-    if(selected_mode==PATTERN_MIDI_MASTER || selected_mode==PATTERN_MIDI_SLAVE || selected_mode==PATTERN_DIN_SLAVE){
-      pattern_count = 0;      //reinitilise le compteur du block pattern
-    }
-    if((selected_mode==SONG_MIDI_MASTER || selected_mode==SONG_MIDI_SLAVE || selected_mode==SONG_DIN_SLAVE)&&(button_reset)){
-      pattern_count = 0;      //reinitilise le compteur de position du song
-    }
-  }
-  PORTB &= ~(1<<2);         // met a 0 la sorti TRIG CPU
+    PORTB &= ~(1<<2);         // met a 0 la sorti TRIG CPU
 }
 
 
 void Check_Midi_Channel() {
-  unsigned int reading = SR.Button_Step_Read();   // on lit les boutons
-  if(reading != old_step_button_state) {            // si ils ont changer on enregistre le temps
-    millis_debounce_step_button = millis();
-  }
-  if((millis()-millis_debounce_step_button) >= DEBOUNCE) {
-    if(reading!=step_button_state) {
-      step_button_state=reading;
-      if(!button_shift) {
-        for (byte i=0;i<16;i++) {//loop autant de fois que de bouton step soit 16
-          if (bitRead (step_button_state,i)) {
-            //on retourne la valeur du pattern selectionner
-            selected_channel = i;
-            selected_channel_changed = 1;//flag que le pattern selectionner a change
-            temp_step_led = (1<<selected_channel);
-            break;
-          }
-        }
-      }
+    unsigned int reading = SR.Button_Step_Read();   // on lit les boutons
+    if(reading != old_step_button_state) {            // si ils ont changer on enregistre le temps
+        millis_debounce_step_button = millis();
     }
-  }
-  old_step_button_state = reading;
+    if((millis()-millis_debounce_step_button) >= DEBOUNCE) {
+        if(reading != step_button_state) {
+            step_button_state = reading;
+            if(!button_shift) {
+                for (byte i=0; i<16; i++) {//loop autant de fois que de bouton step soit 16
+                    if (bitRead (step_button_state,i)) {
+                        //on retourne la valeur du pattern selectionner
+                        selected_channel = i;
+                        selected_channel_changed = 1;//flag que le pattern selectionner a change
+                        temp_step_led = (1<<selected_channel);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    old_step_button_state = reading;
 }
 
 
 void Disconnect_Callback() {
-  /*  
-   DDRA |= ~B11111100;// les PIN 39 et 40 sont en sorti pour le commun des rotary switch les autres sont en entree pour l'encoder et les rotary switch
-   DDRC |= B11111100;// les 6 bits fort du PORTC sont en sorti pour les 6 led Edit
-   DDRB |= B111;// les trois sorties TRIG  CPU,1 et 2
-   DDRD |= (1<<6);//sortie trig out
-   inst_midi_buffer=0;
-   SR.ShiftOut_Update(temp_step_led,inst_midi_buffer);
-   PORTB &=~1<<2;
-   */
-  PORTC &= ~(B11111100);//clear les edits leds dans ce mode 
-  SR.Led_Step_Write(0);//tous les leds Step Off
-  MIDI.disconnectCallbackFromType(NoteOn);
-  MIDI.disconnectCallbackFromType(Clock);
-  MIDI.disconnectCallbackFromType(Start);
-  MIDI.disconnectCallbackFromType(Stop);
+    /*  
+    DDRA |= ~B11111100;// les PIN 39 et 40 sont en sorti pour le commun des rotary switch les autres sont en entree pour l'encoder et les rotary switch
+    DDRC |= B11111100;// les 6 bits fort du PORTC sont en sorti pour les 6 led Edit
+    DDRB |= B111;// les trois sorties TRIG  CPU,1 et 2
+    DDRD |= (1<<6);//sortie trig out
+    inst_midi_buffer=0;
+    SR.ShiftOut_Update(temp_step_led,inst_midi_buffer);
+    PORTB &=~1<<2;
+    */
+    PORTC &= ~(B11111100);//clear les edits leds dans ce mode 
+    SR.Led_Step_Write(0);//tous les leds Step Off
+    MIDI.disconnectCallbackFromType(NoteOn);
+    MIDI.disconnectCallbackFromType(Clock);
+    MIDI.disconnectCallbackFromType(Start);
+    MIDI.disconnectCallbackFromType(Stop);
 }
