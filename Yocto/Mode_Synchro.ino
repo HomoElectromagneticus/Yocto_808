79ac2ec6991defd73522f756f1ef9a80cd07de74
Wim V. (rv0) <rv0soft@gmail.com>
2019-06-09 20:28:19 +0200

    Dinsync handling didn't belong in mode_synchro

--- Yocto/Mode_Synchro.ino
+++ Yocto/Mode_Synchro.ino
@@ -46,21 +46,3 @@ void Mode_Synchro(byte mode) {
     break;
   }
 }
-
-void Set_Dinsync_Clock_High() {
-  PORTD |= (1<<4); // Set the clock high.
-  dinsync_clock_timeout = 4; // 4ms clock.
-}
-
-
-void Set_Dinsync_Clock_Low() {
-  PORTD &= ~(1<<4); // Lower the clock signal.
-}
-
-void Set_Dinsync_Run_High() {
-  PORTD |= (1<<5); // Set Run = 1.
-}
-
-void Set_Dinsync_Run_Low() {
-  PORTD &= ~(1<<5); // Set Run = 0.
-}
